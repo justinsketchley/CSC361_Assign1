cbf5075c77f9352df3e1aa531eca20aa97c1a300
Justin Sketchley <doughnut@n-theta.csc.uvic.ca>
2012-10-09 14:45:13 -0700

    ad

---
+++ link_layer.cpp
@@ -0,0 +1,204 @@
+#include "link_layer.h"
+#include "timeval_operators.h"
+#include <pthread.h>
+#include <sys/time.h>
+
+unsigned short checksum(struct Packet);
+
+void Send_Queue :: enqueue(Timed_packet p)
+{
+	if((head==0 && tail==MAX_QUEUE_SIZE-1) || (tail+1==head)) {
+		//queue full
+	}	
+	else {
+		if(tail==MAX_QUEUE_SIZE-1) {
+			tail=0;
+		}
+		else {
+			tail++;
+		}
+		data[tail]=p;
+	}
+	if(head == -1) {
+		head = 0;
+	}
+}
+Timed_packet Send_Queue :: dequeue()
+{
+	Timed_packet k;
+
+	if(isEmpty()) {
+		//Empty
+	}
+	else {
+		k=data[head];
+		if(head==tail) {
+			head=tail=-1;
+		}
+		else if (head==MAX_QUEUE_SIZE-1) {
+			head=0;
+		}
+		else {
+			head++;
+		}
+	}
+	return k;
+}
+bool Send_Queue :: isEmpty() {
+	if(head == -1) {
+		return true;
+	}
+	return false;
+}
+
+
+Link_layer::Link_layer(Physical_layer_interface* physical_layer_interface,
+ unsigned int num_sequence_numbers,
+ unsigned int max_send_window_size,unsigned int timeout):num_sequence_numbers(num_sequence_numbers),max_send_window_size(max_send_window_size)
+{
+	this->physical_layer_interface = physical_layer_interface;
+
+	receive_buffer_length = 0;
+
+	next_send_seq = 0;
+	next_send_ack = 0;
+	next_receive_seq = 0;
+	last_receive_ack = 0;
+
+	pthread_mutex_init(&lock,NULL);
+	if (pthread_create(&thread,NULL,&Link_layer::loop,this) < 0) {
+		throw Link_layer_exception();
+	}
+
+}
+
+unsigned int Link_layer::send(unsigned char buffer[],unsigned int length)
+{
+	if(length == 0 || length > max_send_window_size) {
+		throw Link_layer_exception();
+	}
+	if(!send_queue.isEmpty()) {
+		Timed_packet P;
+		gettimeofday(&P.send_time,NULL);
+		std::copy(buffer,buffer+sizeof(buffer),P.packet.data);
+		P.packet.header.data_length = length;
+		P.packet.header.seq = next_send_seq;
+		send_queue.enqueue(P);
+
+		next_send_seq++;
+
+		//now return true		
+	}
+	//Remove this	
+	unsigned int n = physical_layer_interface->send(buffer,length);
+	//Change to return false
+	return n;
+}
+
+unsigned int Link_layer::receive(unsigned char buffer[])
+{
+	unsigned int length;
+
+	pthread_mutex_lock(&lock);
+	length = receive_buffer_length;
+	if (length > 0) {
+		for (unsigned int i = 0; i < length; i++) {
+			buffer[i] = receive_buffer[i];
+		}
+		receive_buffer_length = 0;
+	}
+	pthread_mutex_unlock(&lock);
+
+	return length;
+}
+
+void Link_layer::process_received_packet(struct Packet p)
+{
+	if (p.header.seq == next_receive_seq) {
+		if (sizeof(p.data) > 0) {
+			pthread_mutex_lock(&lock);
+			if (receive_buffer_length == 0){
+				//copy packet data to receive_buffer
+				std::copy(p.data,p.data+sizeof(p.data),receive_buffer);
+				//increment next_receive_seq
+				next_receive_seq++;
+			}
+			pthread_mutex_unlock(&lock);
+		}
+		else {
+			//increment next_receive_seq
+			next_receive_seq++;
+		}
+	}
+	last_receive_ack = p.header.ack;
+}
+
+void Link_layer::remove_acked_packets()
+{
+	//if send_queue contains a packet acknowldged by last_receive_ack
+	//if(send_queue.
+		//remove that packet and all packets to its left
+}
+
+void Link_layer::send_timed_out_packets()
+{
+}
+
+void Link_layer::generate_ack_packet()
+{
+}
+
+void* Link_layer::loop(void* thread_creator)
+{
+	const unsigned int LOOP_INTERVAL = 10;
+	Link_layer* link_layer = ((Link_layer*) thread_creator);
+
+	while (true) {
+		if (link_layer->receive_buffer_length == 0) {
+			unsigned int length =
+			 link_layer->physical_layer_interface->receive
+			 (link_layer->receive_buffer);
+
+			if (length > 0) {
+				link_layer->receive_buffer_length = length;
+			}
+		}
+
+		usleep(LOOP_INTERVAL);
+	}
+
+	return NULL;
+}
+
+
+
+// this is the standard Internet checksum algorithm
+unsigned short checksum(struct Packet p)
+{
+	unsigned long sum = 0;
+	struct Packet copy;
+	unsigned short* shortbuf;
+	unsigned int length;
+
+	if (p.header.data_length > Link_layer::MAXIMUM_DATA_LENGTH) {
+		throw Link_layer_exception();
+	}
+
+	copy = p;
+	copy.header.checksum = 0;
+	length = sizeof(Packet_header)+copy.header.data_length;
+	shortbuf = (unsigned short*) &copy;
+
+	while (length > 1) {
+		sum += *shortbuf++;
+		length -= 2;
+	}
+	// handle the trailing byte, if present
+	if (length == 1) {
+		sum += *(unsigned char*) shortbuf;
+	}
+
+	sum = (sum >> 16)+(sum & 0xffff);
+	sum = (~(sum+(sum >> 16)) & 0xffff);
+	return (unsigned short) sum;
+}
---
+++ link_layer.h
@@ -0,0 +1,87 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <exception>
+#include <queue>
+
+#include "physical_layer.h"
+
+class Link_layer_exception: public exception
+{};
+
+struct Packet_header {
+	unsigned int checksum;
+	unsigned int seq;
+	unsigned int ack;
+	unsigned int data_length;
+};
+
+struct Packet {
+	struct Packet_header header;
+	unsigned char data[Physical_layer_interface::MAXIMUM_BUFFER_LENGTH-
+	 sizeof(struct Packet_header)];
+};
+
+struct Timed_packet {
+	timeval send_time;
+	struct Packet packet;
+};
+
+const int MAX_QUEUE_SIZE = 1024;
+class Send_Queue {
+
+
+	Timed_packet data[MAX_QUEUE_SIZE];
+	int head,tail;
+	public :
+	Send_Queue()
+	{
+		head=tail=-1;
+	}
+	void enqueue(Timed_packet);
+	Timed_packet dequeue();
+	bool isEmpty();
+};
+
+class Link_layer {
+public:
+	enum {MAXIMUM_DATA_LENGTH = 
+	 Physical_layer_interface::MAXIMUM_BUFFER_LENGTH-sizeof(Packet_header)};
+
+	Link_layer(Physical_layer_interface* physical_layer_interface,
+	 unsigned int num_sequence_numbers,
+	 unsigned int max_send_window_size,unsigned int timeout);
+
+	unsigned int send(unsigned char buffer[], unsigned int length);
+
+	unsigned int receive(unsigned char buffer[]);
+private:
+	Physical_layer_interface* physical_layer_interface;
+	unsigned int num_sequence_numbers;
+	unsigned int max_send_window_size;
+	timeval timeout;
+
+	pthread_t thread;
+	pthread_mutex_t lock;
+
+	// seq for next packet added to send_queue
+	unsigned int next_send_seq;
+
+	unsigned int next_send_ack;
+
+	// seq of next packet expected from PL
+	unsigned int next_receive_seq;
+
+	// last ack received from PL
+	unsigned int last_receive_ack;
+
+	unsigned char receive_buffer[MAXIMUM_DATA_LENGTH];
+	unsigned int receive_buffer_length;
+
+	Send_Queue send_queue;
+
+	static void* loop(void* link_layer);
+	void process_received_packet(struct Packet p);
+	void remove_acked_packets();
+	void send_timed_out_packets();
+	void generate_ack_packet();
+};
